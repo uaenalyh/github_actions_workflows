54a8ef4aa4643e3dfa505733e1c73fe221a3d538
Junjie Mao <junjie.mao@intel.com>
2019-10-13 20:27:47 +0800

    slice: qemu.mk: add virtual USB and ethernet to the Qemu-emulated machine
    Signed-off-by: Junjie Mao <junjie.mao@intel.com>

--- libs/qemu.mk
+++ libs/qemu.mk
@@ -10,7 +10,8 @@ LINUX_ROOTFS ?= clear-31090-kvm.img
 UT_CASE ?= xsave
 UT_CASE_2 ?= tsc_adjust
 
-ZEPHYR_BINARY := $(ZEPHYR_DIR)/zephyr/samples/$(ZEPHYR_SAMPLE)/build/zephyr/zephyr.bin
+ZEPHYR_ELF := $(ZEPHYR_DIR)/zephyr/samples/$(ZEPHYR_SAMPLE)/build/zephyr/zephyr.elf
+ZEPHYR_BINARY := $(subst .elf,.bin,$(ZEPHYR_ELF))
 LINUX_BZIMAGE_BINARY := $(LINUX_DIR)/build/arch/x86/boot/bzImage
 LINUX_ROOTFS_IMAGE := $(LINUX_DIR)/$(LINUX_ROOTFS)
 UT_BINARY := $(UT_DIR)/guest/x86/$(UT_CASE).bzimage
@@ -25,12 +26,21 @@ QEMUOPTS += -m 4G -smp cpus=8,cores=4,threads=2 -enable-kvm
 QEMUOPTS += -device isa-debug-exit -device intel-iommu,intremap=on,caching-mode=on,device-iotlb=on
 QEMUOPTS += -debugcon file:/dev/stdout
 QEMUOPTS += -serial mon:stdio -display none
+QEMUOPTS += -drive file=$(LINUX_ROOTFS_IMAGE),index=0,format=qcow2,id=rootfs,if=none
+QEMUOPTS += -device nec-usb-xhci,id=xhci
+QEMUOPTS += -device usb-storage,bus=xhci.0,drive=rootfs
+QEMUOPTS += -nic tap,model=e1000,script=no,downscript=no,ifname=zeth
 
 ifndef UT
 $(HV_OBJDIR)/$(HV_FILE).iso: all $(ZEPHYR_BINARY) $(LINUX_BZIMAGE_BINARY)
 	$(GRUBISO) -t 1 $(HV_OBJDIR)/$(HV_FILE).iso $(HV_OBJDIR)/$(HV_FILE).32.out \
 		$(ZEPHYR_BINARY):zephyr \
 		$(LINUX_BZIMAGE_BINARY):linux
+
+.PHONY: qemu_linux
+qemu_linux: $(LINUX_BZIMAGE_BINARY) $(LINUX_ROOTFS_IMAGE)
+	$(QEMUEXE) $(QEMUOPTS) -kernel $(LINUX_BZIMAGE_BINARY) -append "rw root=/dev/sda1 rootwait console=ttyS0 ignore_loglevel tsc=reliable intel_iommu=off noxsave" || true
+
 else
 $(UT_DIR)/guest/x86/%.bzimage:
 	make -C $(UT_DIR)/guest x86/$*.bzimage
@@ -43,7 +53,7 @@ endif
 
 .PHONY: qemu
 qemu: $(HV_OBJDIR)/$(HV_FILE).iso
-	$(QEMUEXE) $(QEMUOPTS) -cdrom $(HV_OBJDIR)/$(HV_FILE).iso || true
+	$(QEMUEXE) $(QEMUOPTS) -boot d -cdrom $(HV_OBJDIR)/$(HV_FILE).iso || true
 
 .PHONY: qemu-debug
 qemu-debug: $(HV_OBJDIR)/$(HV_FILE).iso
