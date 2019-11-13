19125b9af4a7925e4b57178047e2a31d44e09c47
Binbin Wu <binbin.wu@intel.com>
2019-11-13 10:09:44 +0800

    hv: config: add an option to disable mce on psc workaround
    Add a option MCE_ON_PSC_WORKAROUND_DISABLED to disable the software
    workaround for the issue Machine Check Error on Page Size Change.
    
    Tracked-On: #4101
    Signed-off-by: Binbin Wu <binbin.wu@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/arch/x86/security.c
+++ hypervisor/arch/x86/security.c
@@ -169,6 +169,12 @@ void set_fs_base(void)
 }
 #endif
 
+#ifdef CONFIG_MCE_ON_PSC_WORKAROUND_DISABLED
+bool is_ept_force_4k_ipage(void)
+{
+	return false;
+}
+#else
 bool is_ept_force_4k_ipage(void)
 {
 	bool force_4k_ipage = true;
@@ -213,3 +219,4 @@ bool is_ept_force_4k_ipage(void)
 
 	return force_4k_ipage;
 }
+#endif
