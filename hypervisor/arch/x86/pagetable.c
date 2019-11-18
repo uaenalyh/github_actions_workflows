3b9e27089c1841c6b65310bc6111c15bdc898113
Binbin Wu <binbin.wu@intel.com>
2019-11-18 15:15:16 +0800

    hv: ept: build 4KB page mapping in EPT for RTVM for MCE on PSC
    Deterministic is important for RTVM. The mitigation for MCE on
    Page Size Change converts a large page to 4KB pages runtimely during
    the vmexit triggered by the instruction fetch in the large page.
    These vmexits increase nondeterminacy, which should be avoided for RTVM.
    This patch builds 4KB page mapping in EPT for RTVM to avoid these vmexits.
    
    Tracked-On: #4101
    Signed-off-by: Binbin Wu <binbin.wu@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- hypervisor/arch/x86/pagetable.c
+++ hypervisor/arch/x86/pagetable.c
@@ -292,8 +292,8 @@ static void add_pde(const uint64_t *pdpte, uint64_t paddr_start, uint64_t vaddr_
 			pr_fatal("%s, pde 0x%lx is already present!\n", __func__, vaddr);
 		} else {
 			if (mem_ops->pgentry_present(*pde) == 0UL) {
-				if (mem_aligned_check(paddr, PDE_SIZE) && mem_aligned_check(vaddr, PDE_SIZE) &&
-					(vaddr_next <= vaddr_end)) {
+				if (mem_ops->large_page_enabled && mem_aligned_check(paddr, PDE_SIZE) &&
+					mem_aligned_check(vaddr, PDE_SIZE) && (vaddr_next <= vaddr_end)) {
 					mem_ops->tweak_exe_right(&prot);
 					set_pgentry(pde, paddr | (prot | PAGE_PSE), mem_ops);
 					if (vaddr_next < vaddr_end) {
@@ -338,8 +338,8 @@ static void add_pdpte(const uint64_t *pml4e, uint64_t paddr_start, uint64_t vadd
 			pr_fatal("%s, pdpte 0x%lx is already present!\n", __func__, vaddr);
 		} else {
 			if (mem_ops->pgentry_present(*pdpte) == 0UL) {
-				if (mem_aligned_check(paddr, PDPTE_SIZE) && mem_aligned_check(vaddr, PDPTE_SIZE) &&
-					(vaddr_next <= vaddr_end)) {
+				if (mem_ops->large_page_enabled && mem_aligned_check(paddr, PDPTE_SIZE) &&
+					mem_aligned_check(vaddr, PDPTE_SIZE) && (vaddr_next <= vaddr_end)) {
 					mem_ops->tweak_exe_right(&prot);
 					set_pgentry(pdpte, paddr | (prot | PAGE_PSE), mem_ops);
 					if (vaddr_next < vaddr_end) {
