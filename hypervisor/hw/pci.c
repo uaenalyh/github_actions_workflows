d6c82198debcd652485198608daed5901e2af79d
Cao Minggui <minggui.cao@intel.com>
2020-03-27 14:36:48 +0800

    MD: add comments to pci module
    1. add comments to pci.c and pci.h
    2. remove unused macro definitions.
    
    Signed-off-by: Cao Minggui <minggui.cao@intel.com>

--- hypervisor/hw/pci.c
+++ hypervisor/hw/pci.c
@@ -45,83 +45,262 @@
 /**
  * @defgroup hwmgmt_pci hwmgmt.pci
  * @ingroup hwmgmt
- * @brief {TBD brief description}
+ * @brief Implementation of all external APIs to operate PCI configuration registers of a physical PCI device.
  *
- * {TBD detailed description, including purposes, designed usages, usage remarks and dependency justification}
+ * This module implements the APIs to operate PCI configuration registers of a physical PCI device. The main functions
+ * include: read a register from PCI configuration space and write a register to PCI configuration space.
+ *
+ * Usage:
+ * - 'vp-dm.vperipheral' module depends on this module to operate the physical PCI device associated with the
+ * virtual PCI device.
+ *
+ * Dependency:
+ * - This module depends on 'lib.lock' module to do spin lock and unlock for PCI devices operation.
+ * - This module depends on 'hwmgmt.io' module to do port IO read/write operations.
  *
  * @{
  */
 
 /**
  * @file
- * @brief {TBD brief description}
+ * @brief This file implements all external APIs that shall be provided by the hwmgmt.pci module.
  *
- * {TBD detailed description, including purposes, designed usages, usage remarks and dependency justification}
+ * This file implements all external functions, data structures, and macros that shall be provided by the
+ * hwmgmt.pci module.
+ *
+ * It also defines a helper functions:
+ * - pci_pdev_calc_address: calculate the address used to do port I/O operation for PCI configuration registers.
+ *
  */
 
+/**
+ * @brief A spin lock variable used in PCI read/write operation, to avoid different guest VMs to operate the PCI
+ * configuration space in parallel.
+ */
 static spinlock_t pci_device_lock = {.head = 0U, .tail = 0U};
 
+/**
+ * @brief Calculate an address as PCI spec requirement and used to operate a PCI device configuration register
+ *
+ * This function is called to calculate an address as PCI spec requirement and the address can be used to
+ * operate a PCI device configuration register. The 32bits address format is:
+ * - bit 0-7: the register offset in the PCI configuration space
+ * - bit 8-23: the BDF number of the PCI device
+ * - bit 31: the enable bit, set it to 1 to make this address valid
+ * Other bits are set to 0.
+ *
+ * @param[in] bdf The BDF number of the PCI device.
+ * @param[in] offset The register offset in the PCI configuration space.
+ *
+ * @return the address used to read/write PCI configuration register
+ *
+ * @pre offset < PCI_REGMAX
+ *
+ * @post N/A
+ *
+ * @mode HV_INIT, HV_OPERATIONAL
+ *
+ * @remark It is an internal function called by pci_pdev_read_cfg and pci_pdev_write_cfg
+ *
+ * @reentrancy unspecified
+ * @threadsafety Yes
+ */
 static uint32_t pci_pdev_calc_address(union pci_bdf bdf, uint32_t offset)
 {
+	/** Declare the following local variables of type uint32_t.
+	 *  - addr representing the address to be returned, initialized as bdf.value. */
 	uint32_t addr = (uint32_t)bdf.value;
 
+	/** Shift 'addr' left for 8 bits, in order to move BDF value to bits: 8-23 */
 	addr <<= 8U;
+	/** Bitwise OR 'addr' by 'offset' OR PCI_CFG_ENABLE, in order to set bit0-7 to 'offset', and bit32 to 1 */
 	addr |= (offset | PCI_CFG_ENABLE);
+	/** Return the address used to read/write PCI configuration register */
 	return addr;
 }
 
+/**
+ * @brief Read a register from the physical PCI device configuration space
+ *
+ * This function is called to read a register from the physical PCI device configuration space. It calculates an
+ * addressable value according to the input parameters of the PCI device: BDF, offset and bytes. And write the
+ * address to the PCI configuration-address port, then read the register value from the PCI configuration-data port.
+ *
+ * @param[in] bdf The BDF number of the PCI device.
+ * @param[in] offset The register offset in the PCI configuration space.
+ * @param[in] bytes The length to read.
+ *
+ * @return The register value of the phyiscal PCI device in its configuration space.
+ *
+ * @pre offset < PCI_REGMAX
+ * @pre bytes shall be the value among 1, 2 and 4.
+ *
+ * @post N/A
+ *
+ * @mode HV_INIT, HV_OPERATIONAL
+ *
+ * @remark It is a public API called by other modules.
+ *
+ * @reentrancy unspecified
+ * @threadsafety Yes
+ */
 uint32_t pci_pdev_read_cfg(union pci_bdf bdf, uint32_t offset, uint32_t bytes)
 {
+	/** Declare the following local variables of type uint32_t.
+	 *  - addr representing the value to address the register of the physical PCI device, not initialized. */
 	uint32_t addr;
+	/** Declare the following local variables of type uint32_t.
+	 *  - val representing the value of the register to read, not initialized. */
 	uint32_t val;
 
+	/** Call pci_pdev_calc_address with the following parameters, in order to calculate the value
+	 *  for PCI configuration-address port to access the given register, and set addr to its return value.
+	 *  - bdf
+	 *  - offset
+	 */
 	addr = pci_pdev_calc_address(bdf, offset);
 
+	/** Call spinlock_obtain with the following parameters, in order to block other access to the PCI configuration
+	 *  space, and to avoid different guest VMs to operate the PCI configuration space in parallel.
+	 *  - &pci_device_lock
+	 */
 	spinlock_obtain(&pci_device_lock);
 
-	/* Write address to ADDRESS register */
+	/** Call pio_write32 with the following parameters, in order to write 'addr' to PCI configuration-address port.
+	 *  - addr
+	 *  - PCI_CONFIG_ADDR
+	 */
 	pio_write32(addr, (uint16_t)PCI_CONFIG_ADDR);
 
-	/* Read result from DATA register */
+	/** Depending on the value of 'bytes': 1, 2 or others (4 bytes aligned as default) */
 	switch (bytes) {
+	/** To read \a bytes is 1 */
 	case 1U:
+		/** Set val to the value returned by pio_read8 with following parameter, to read one byte from the
+		 *  physical PCI configure data port.
+		 *  - (uint16_t)PCI_CONFIG_DATA + ((uint16_t)offset & 3U)
+		 */
 		val = (uint32_t)pio_read8((uint16_t)PCI_CONFIG_DATA + ((uint16_t)offset & 3U));
+		/** End of case */
 		break;
+		/** To read \a bytes is 2 */
 	case 2U:
+		/** Set val to the value returned by pio_read16 with following parameter, to read two bytes from the
+		 *  physical PCI configure data port.
+		 *  - (uint16_t)PCI_CONFIG_DATA + ((uint16_t)offset & 2U)
+		 */
 		val = (uint32_t)pio_read16((uint16_t)PCI_CONFIG_DATA + ((uint16_t)offset & 2U));
+		/** End of case */
 		break;
+	/** Otherwise */
 	default:
+		/** Set val to the value returned by pio_read32 with following parameter, to read four bytes from the
+		 *  physical PCI configure data port.
+		 *  - (uint16_t)PCI_CONFIG_DATA
+		 */
 		val = pio_read32((uint16_t)PCI_CONFIG_DATA);
+		/** End of case */
 		break;
 	}
+	/** Call spinlock_release with the following parameters, in order to unlock the access to PCI configuration
+	 *  space.
+	 *  - &pci_device_lock
+	 */
 	spinlock_release(&pci_device_lock);
 
+	/** Return 'val' which is the register value. */
 	return val;
 }
 
+/**
+ * @brief Write a value to a configuration register of the physical PCI device
+ *
+ * This function is called to write a value to a configuration register of the physical PCI device. It calculates an
+ * addressable value according to the input parameters of the PCI device: BDF, offset and bytes. And write the
+ * address to the PCI configuration-address port, then write the register value to the PCI configuration-data port.
+ *
+ * @param[in] bdf The BDF number of the PCI device.
+ * @param[in] offset The register offset in the PCI configuration space.
+ * @param[in] bytes The length to write.
+ * @param[in] val The value to write.
+ *
+ * @return None
+ *
+ * @pre offset < PCI_REGMAX
+ * @pre bytes shall be the value among 1, 2 and 4.
+ *
+ * @post N/A
+ *
+ * @mode HV_INIT, HV_OPERATIONAL
+ *
+ * @remark It is a public API called by other modules.
+ *
+ * @reentrancy unspecified
+ * @threadsafety Yes
+ */
 void pci_pdev_write_cfg(union pci_bdf bdf, uint32_t offset, uint32_t bytes, uint32_t val)
 {
+	/** Declare the following local variables of type uint32_t.
+	 *  - addr representing the value to address the register of the physical PCI device, not initialized. */
 	uint32_t addr;
 
+	/** Call spinlock_obtain with the following parameters, in order to block other access to the PCI configuration
+	 *  space, and to avoid different guest VMs to operate the PCI configuration space in parallel.
+	 *  - &pci_device_lock
+	 */
 	spinlock_obtain(&pci_device_lock);
 
+	/** Call pci_pdev_calc_address with the following parameters, in order to calculate the value
+	 *  for PCI configuration-address port to access the given register, and set addr to its return value.
+	 *  - bdf
+	 *  - offset
+	 */
 	addr = pci_pdev_calc_address(bdf, offset);
 
-	/* Write address to ADDRESS register */
+	/** Call pio_write32 with the following parameters, in order to write 'addr' to PCI configuration-address port.
+	 *  - addr
+	 *  - PCI_CONFIG_ADDR
+	 */
 	pio_write32(addr, (uint16_t)PCI_CONFIG_ADDR);
 
-	/* Write value to DATA register */
+	/** Depending on the value of 'bytes' */
 	switch (bytes) {
+	/** To write \a bytes is 1 */
 	case 1U:
+		/** Call pio_write8 with the following parameters, in order to write 'val' to the configuration register
+		 *  of the physical PCI device.
+		 *  - (uint8_t)val
+		 *  - (uint16_t)PCI_CONFIG_DATA + ((uint16_t)offset & 3U)
+		 */
 		pio_write8((uint8_t)val, (uint16_t)PCI_CONFIG_DATA + ((uint16_t)offset & 3U));
+		/** End of case */
 		break;
+		/** To write \a bytes is 2 */
 	case 2U:
+		/** Call pio_write16 with the following parameters, in order to write 'val' to the register in the
+		 *  configuration space.
+		 *  of the physical PCI device.
+		 *  - (uint16_t)val
+		 *  - (uint16_t)PCI_CONFIG_DATA + ((uint16_t)offset & 2U)
+		 */
 		pio_write16((uint16_t)val, (uint16_t)PCI_CONFIG_DATA + ((uint16_t)offset & 2U));
+		/** End of case */
 		break;
+	/** Otherwise */
 	default:
+		/** Call pio_write32 with the following parameters, in order to write 'val' to the configuration
+		 *  register of the physical PCI device.
+		 *  - val
+		 *  - (uint16_t)PCI_CONFIG_DATA
+		 */
 		pio_write32(val, (uint16_t)PCI_CONFIG_DATA);
+		/** End of case */
 		break;
 	}
+	/** Call spinlock_release with the following parameters, in order to unlock the access to PCI configuration
+	 *  space.
+	 *  - &pci_device_lock
+	 */
 	spinlock_release(&pci_device_lock);
 }
 
--- hypervisor/include/hw/pci.h
+++ hypervisor/include/hw/pci.h
@@ -39,98 +39,138 @@
 
 /**
  * @file
- * @brief {TBD brief description}
+ * @brief This file declares all external APIs that shall be provided by the hwmgmt.pci module.
  *
- * {TBD detailed description, including purposes, designed usages, usage remarks and dependency justification}
+ * This file declares all external functions, data structures, and macros that shall be provided by the
+ * hwmgmt.pci module.
+ *
+ * For the macros definition of PCI configuration space registers, there are some rules as following:
+ * - PCIM_xxx: mask to locate subfield in register
+ * - PCIR_xxx: configuration register offset
+ * - PCIC_xxx: device class
+ * - PCIS_xxx: device subclass
  */
 
-/*
- * PCIM_xxx: mask to locate subfield in register
- * PCIR_xxx: config register offset
- * PCIC_xxx: device class
- * PCIS_xxx: device subclass
- * PCIP_xxx: device programming interface
- * PCIV_xxx: PCI vendor ID (only required to fixup ancient devices)
- * PCID_xxx: device ID
- * PCIY_xxx: capability identification number
- * PCIZ_xxx: extended capability identification number
- */
+#define PCI_BAR_COUNT 0x6U /**< Pre-defined PCI devices BAR count. */
+#define PCI_REGMAX    0xFFU /**< Pre-defined the MAX offset of the PCI configuration space registers. */
 
-#define PCI_BAR_COUNT 0x6U
-#define PCI_REGMAX    0xFFU
+#define PCI_CONFIG_ADDR 0xCF8U /**< Pre-defined the I/O port of PCI configuration address. */
+#define PCI_CONFIG_DATA 0xCFCU /**< Pre-defined the I/O port of PCI configuration data. */
 
-/* I/O ports */
-#define PCI_CONFIG_ADDR 0xCF8U
-#define PCI_CONFIG_DATA 0xCFCU
-
-#define PCI_CFG_ENABLE 0x80000000U
+#define PCI_CFG_ENABLE 0x80000000U /**< Pre-defined the mask used when accessing PCI configuration space registers. */
 
 /* PCI config header registers for all devices */
-#define PCIR_VENDOR               0x00U
-#define PCIR_DEVICE               0x02U
-#define PCIR_COMMAND              0x04U
-#define PCIM_CMD_INTxDIS          0x400U
-#define PCIR_REVID                0x08U
-#define PCIR_SUBCLASS             0x0AU
-#define PCIR_CLASS                0x0BU
-#define PCIR_HDRTYPE              0x0EU
-#define PCIM_HDRTYPE_NORMAL       0x00U
-#define PCIM_MFDEV                0x80U
-#define PCIR_BARS                 0x10U
-#define PCIM_BAR_SPACE            0x01U
-#define PCIM_BAR_IO_SPACE         0x01U
-#define PCIM_BAR_MEM_TYPE         0x06U
-#define PCIM_BAR_MEM_32           0x00U
-#define PCIM_BAR_MEM_64           0x04U
-#define PCIR_CAP_PTR              0x34U
-#define PCI_BASE_ADDRESS_MEM_MASK (~0x0fUL)
-#define PCI_BASE_ADDRESS_IO_MASK  (~0x03UL)
+#define PCIR_VENDOR          0x00U /**< Pre-defined the offset of vendor ID register in PCI configuration space. */
+#define PCIR_DEVICE          0x02U /**< Pre-defined the offset of device ID register in PCI configuration space. */
+#define PCIR_COMMAND         0x04U /**< Pre-defined the offset of command register in PCI configuration space. */
+#define PCIM_CMD_INTxDIS     0x400U /**< Pre-defined the mask used to set disable bit to PCI legacy interrupt. */
+#define PCIR_REVID           0x08U /**< Pre-defined the offset of revision ID register in PCI configuration space. */
+#define PCIR_SUBCLASS        0x0AU /**< Pre-defined the offset of subclass ID register in PCI configuration space. */
+#define PCIR_CLASS           0x0BU /**< Pre-defined the offset of class ID register in PCI configuration space. */
+#define PCIR_HDRTYPE         0x0EU /**< Pre-defined the offset of header type register in PCI configuration space. */
+#define PCIM_HDRTYPE_NORMAL  0x00U /**< Pre-defined the mask used as to define the type of PCI configuration space */
+#define PCIR_BARS            0x10U /**< Pre-defined the offset of BAR registers in PCI configuration space.*/
+#define PCIM_BAR_SPACE       0x01U /**< Pre-defined the mask used to check PCI BAR type: MMIO or I/O. */
+#define PCIM_BAR_IO_SPACE    0x01U /**< Pre-defined the mask used to indicate the BAR type: I/O mapped. */
+#define PCIM_BAR_MEM_TYPE    0x06U /**< Pre-defined the mask used to check the MMIO BAR is 32 or 64bits. */
+#define PCIM_BAR_MEM_32      0x00U /**< Pre-defined the mask used to indicate the MMIO BAR is 32bits. */
+#define PCIM_BAR_MEM_64      0x04U /**< Pre-defined the mask used to indicate the MMIO BAR is 64bits. */
+#define PCIR_CAP_PTR         0x34U /**< Pre-defined the offset of capability register in PCI configuration space. */
+
+#define PCI_BASE_ADDRESS_MEM_MASK (~0x0fUL) /**< Pre-defined the mask used to calculate base address of a MMIO BAR */
 
 /* Capability Register Offsets */
-#define PCICAP_ID      0x0U
-#define PCICAP_NEXTPTR 0x1U
+#define PCICAP_ID      0x0U /**< Pre-defined the offset of capability ID register within a capability structure. */
+#define PCICAP_NEXTPTR 0x1U /**< Pre-defined the offset of next capability register within a capability structure. */
 
 /* Capability Identification Numbers */
-#define PCIY_MSI 0x05U
+#define PCIY_MSI 0x05U /**< Pre-defined the ID number of MSI capability. */
 
 /* PCI Message Signalled Interrupts (MSI) */
-#define PCIR_MSI_CTRL           0x02U
-#define PCIM_MSICTRL_64BIT      0x80U
-#define PCIM_MSICTRL_MSI_ENABLE 0x01U
-#define PCIR_MSI_ADDR           0x4U
-#define PCIR_MSI_ADDR_HIGH      0x8U
-#define PCIR_MSI_DATA           0x8U
-#define PCIR_MSI_DATA_64BIT     0xCU
+#define PCIR_MSI_CTRL           0x02U /**< Pre-defined the offset of MSI control register within MSI capability */
+#define PCIM_MSICTRL_64BIT      0x80U /**< Pre-defined the mask used to check the MSI address/data is 64bits */
+#define PCIM_MSICTRL_MSI_ENABLE 0x01U /**< Pre-defined the mask used to enable/disable MSI */
+#define PCIR_MSI_ADDR           0x4U /**< Pre-defined the offset of MSI address register within MSI capability */
+#define PCIR_MSI_ADDR_HIGH      0x8U /**< Pre-defined the offset of high 32bits MSI address register. */
+#define PCIR_MSI_DATA           0x8U /**< Pre-defined the offset of MSI data register within MSI capability */
+#define PCIR_MSI_DATA_64BIT     0xCU /**< Pre-defined the offset of high 32bits MSI data register. */
+
+/**
+ * @brief Pre-defined the mask of "multiple message capable" fields in MSI control register.
+ */
 #define PCIM_MSICTRL_MMC_MASK   0x000EU
+/**
+ * @brief Pre-defined the mask of "multiple message enable" fields in MSI control register.
+ */
 #define PCIM_MSICTRL_MME_MASK   0x0070U
 
 /* PCI device class */
-#define PCIC_BRIDGE      0x06U
-#define PCIS_BRIDGE_HOST 0x00U
+#define PCIC_BRIDGE      0x06U /**< Pre-defined the class value for PCI host bridge. */
+#define PCIS_BRIDGE_HOST 0x00U /**< Pre-defined the subclass value for PCI host bridge. */
 
+/**
+ * @brief Data structure to present PCI BDF information
+ *
+ * Data structure to present PCI BDF information, a 16 bits number including BUS (8bits)/DEVICE (5bits)/
+ * FUNCTION (3bits), or combine DEVICE and FUNCTION as a 8bits number.
+ *
+ * @consistency N/A
+ * @alignment N/A
+ *
+ * @remark N/A
+ */
 union pci_bdf {
-	uint16_t value;
+	uint16_t value; /**< A 16bits number combined BUS, DEVICE and FUNCTION. */
 	struct {
-		uint8_t f : 3; /* BITs 0-2 */
-		uint8_t d : 5; /* BITs 3-7 */
-		uint8_t b; /* BITs 8-15 */
+		uint8_t f : 3; /**< BITs 0-2 as FUNCTION number of a PCI device */
+		uint8_t d : 5; /**< BITs 3-7 as DEVICE number of a PCI device */
+		uint8_t b; /**< BITs 8-15 as BUS number of a PCI device */
 	} bits;
 	struct {
-		uint8_t devfun; /* BITs 0-7 */
-		uint8_t bus; /* BITs 8-15 */
+		uint8_t devfun; /**< BITs 0-7 combined DEVICE and FUNCTION numbers */
+		uint8_t bus; /**< BITs 8-15 as BUS number of a PCI device */
 	} fields;
 };
 
+/**
+ * @brief An enumeration type to indicate a BAR register's type.
+ *
+ * This enumeration type represents the BAR register's types, including IO BAR and MMIO BAR (32bits or 64bits).
+ *
+ * @remark N/A
+ */
 enum pci_bar_type {
-	PCIBAR_NONE = 0,
-	PCIBAR_IO_SPACE,
-	PCIBAR_MEM32,
-	PCIBAR_MEM64,
-	PCIBAR_MEM64HI,
+	PCIBAR_NONE = 0, /**< the value to indicate no BAR included in the PCI device */
+	PCIBAR_IO_SPACE, /**< IO BAR */
+	PCIBAR_MEM32, /**< 32bits MMIO BAR */
+	PCIBAR_MEM64, /**< 64bits MMIO BAR */
+	PCIBAR_MEM64HI, /**< the high 32bits part of a 64bits MMIO BAR */
 };
 
+/**
+ * @brief Get the offset of a BAR register in PCI configuration space.
+ *
+ * This function is called to get the offset of a BAR register in PCI configuration space.
+ *
+ * @param[in] idx The index of the BAR register in PCI configuration space.
+ *
+ * @return the offset of the BAR register corresponding with the given 'idx'
+ *
+ * @pre idx < PCI_BAR_COUNT
+ *
+ * @post N/A
+ *
+ * @mode HV_INIT, HV_OPERATIONAL
+ *
+ * @remark N/A
+ *
+ * @reentrancy unspecified
+ * @threadsafety Yes
+ */
 static inline uint32_t pci_bar_offset(uint32_t idx)
 {
+	/** Return the sum of PCIR_BARS and idx left-shifted by 2, which is the offset of the (idx + 1)'th BAR
+	 *  in the PCI configuration space */
 	return PCIR_BARS + (idx << 2U);
 }
 
